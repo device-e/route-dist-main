de88fc4ec413d5e8dd8f61e8d1b6e0bc91789139
device-e <zhuangchengjing@foxmail.com>
2026-04-16 19:08:56 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>        {/* ③ 包裹HelmetProvider */}
-      <BrowserRouter basename="/route-dist">  {/* ④ basename与GitHub Pages仓库名一致 */}
+      <BrowserRouter basename="/route-dist-main">  {/* ④ basename与GitHub Pages仓库名一致 */}
         <App />
       </BrowserRouter>
     </HelmetProvider>
